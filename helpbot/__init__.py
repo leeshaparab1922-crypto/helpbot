43a6e82133c209dae4fcb14e27fcab5c3a92a706
leeshaparab1922-crypto <leeshaparab1922@gmail.com>
2026-06-21 09:52:35 +0530

    Fixes #4: Given system identity and code structure improvement

--- helpbot/__init__.py
+++ helpbot/__init__.py
@@ -1,4 +1,5 @@
 from helpbot.config import Settings
+from helpbot.chat import Helpbot
 from helpbot.conversation import Conversation
 #let there whatever be in helpbot package only import Settings from helpbot.config and not from helpbot
-__all__ = ["Settings", "Conversation"]
+__all__ = ["Settings", "Conversation","Helpbot", "SYSTEM_PROMPT"]
